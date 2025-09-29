a6b69f78b2ab4b50b99d6c190e400dd37b307235
Travis Gooden <tgooden@my.bcit.ca>
2025-09-29 12:05:51 -0700

    fix: change scaling mode to fix behaviour on Windows

--- src/components/GameComponent.tsx
+++ src/components/GameComponent.tsx
@@ -73,7 +73,7 @@ export default function GameComponent() {
 
             // check device
             const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-            const scaleMode = isMobile ? Phaser.Scale.FIT : Phaser.Scale.EXPAND
+            const scaleMode = isMobile ? Phaser.Scale.FIT : Phaser.Scale.WIDTH_CONTROLS_HEIGHT
 
             gameInstance = new Phaser.Game({
                 type: Phaser.WEBGL,
