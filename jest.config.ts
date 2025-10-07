310dc76242f5414b09668b659f4fe3132f34b02a
Travis Gooden <tgooden@my.bcit.ca>
2025-10-06 22:34:02 -0700

    fix: add .test.ts requirement for testing files

--- jest.config.ts
+++ jest.config.ts
@@ -163,10 +163,7 @@ const config: Config = {
     // testLocationInResults: false,
 
     // The glob patterns Jest uses to detect test files
-    // testMatch: [
-    //   "**/__tests__/**/*.?([mc])[jt]s?(x)",
-    //   "**/?(*.)+(spec|test).?([mc])[jt]s?(x)"
-    // ],
+    testMatch: ['**/__tests__/**/*.test.ts'],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
     // testPathIgnorePatterns: [
